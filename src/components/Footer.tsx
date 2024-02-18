340a38281a1654b6f9b37f87fe8b8646fe7962fe
tairoucisse <cisse.amidou@axazara.io>
2024-02-18 23:55:16 +0100

    - layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export function Footer() {
-    return (
+  return (
+    <footer className="bg-black py-7">
+      <div className="flex justify-between items-center md:px-44 px-5">
+        <Image
+          src="/assets/logo-white.svg"
+          width={139}
+          height={40}
+          alt="logo"
+        />
         <div>
-            
+          <span className="text-white">@ 2019-2023 Personal</span>
+          <p className="text-white text-end">Made In Figma</p>
         </div>
-    )
+      </div>
+    </footer>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,24 +2,35 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FiDownload } from "react-icons/fi";
+import { IoMdMenu } from "react-icons/io";
 
 export function Header() {
   return (
-    <nav className="hidden lg:block">
+    <div>
+      <nav className="hidden lg:block">
+        <div className="flex justify-between items-center py-4 px-44">
+          <Image src="/assets/logo.svg" width={139} height={40} alt="logo" />
+          <ul className=" text-heading-04 font-semibold flex gap-6">
+            <Link href="#about-me">About Me</Link>
+            <Link href="#skills">Skills</Link>
+            <Link href="#projects">Project</Link>
+            <Link href="#contact">Contact Me</Link>
+          </ul>
+          <button className="bg-black text-white px-6 py-3 rounded-md flex justify-center items-center gap-2">
+            Resume
+            <FiDownload className="w-4 h-4 text-white" />
+          </button>
+        </div>
+      </nav>
+      {/* Mobile version */}
+      <nav className="px-5 md:hidden block py-4">
+        <div className="flex flex-row justify-between items-center">
 
-    <div className="flex justify-between items-center py-4 px-44">
-      <Image src="/assets/logo.svg" width={139} height={40} alt="logo" />
-      <ul className=" text-heading-04 font-semibold flex gap-6">
-        <Link href="#about-me">About Me</Link>
-        <Link href="#skills">Skills</Link>
-        <Link href="#projects">Project</Link>
-        <Link href="#contact">Contact Me</Link>
-      </ul>
-      <button className="bg-black text-white px-6 py-3 rounded-md flex justify-center items-center gap-2">
-        Resume
-        <FiDownload className="w-4 h-4 text-white" />
-      </button>
+        <Image src="/assets/logo.svg" width={139} height={40} alt="logo" />
+
+        <IoMdMenu className="w-8 h-8" />
+        </div>
+      </nav>
     </div>
-    </nav>
   );
 }
